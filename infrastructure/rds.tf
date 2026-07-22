873c43b4d9485141313544687c8044109247489a
bi86op <bi86op@student.sunderland.ac.uk>
2026-07-22 23:34:25 +0100

    Adjust RDS backup retention to 1 day

--- infrastructure/rds.tf
+++ infrastructure/rds.tf
@@ -45,7 +45,7 @@ resource "aws_db_instance" "medicore" {
   multi_az            = false
 
   # Enables automated backups and point-in-time recovery.
-  backup_retention_period = 7
+  backup_retention_period = 1
   backup_window           = "02:00-03:00"
 
   maintenance_window = "sun:03:00-sun:04:00"
